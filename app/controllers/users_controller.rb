2bd8e4e1cf25aab28eca2c0c51016f73ae430cef
Yu Okuda <yu.kyoto2720@gmail.com>
2018-10-25 15:53:10 +0900

    エラーメッセージの表示機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params #:user属性を必須とし、名前、メールアドレス、パスワード、パスワードの確認の属性をそれぞれ許可し、それ以外を許可しない
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
