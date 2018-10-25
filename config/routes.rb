57d2917c01deb932ca5dc40fb18365a43a916682
Yu Okuda <yu.kyoto2720@gmail.com>
2018-10-25 18:18:17 +0900

    ログインの基礎

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get '/contact' => 'static_pages#contact'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :users
 end
