408a0bfaaec34e9f98bedccef5bc8ff2a9bab7f8
Yu Okuda <yu.kyoto2720@gmail.com>
2018-10-26 17:36:54 +0900

    認可機能の追加、認証されたユーザーだけが情報を変更できたりできるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -45,6 +44,7 @@ class UsersController < ApplicationController
       # ログイン済みユーザーかどうか確認
       def logged_in_user
         unless logged_in?
+          store_location
           flash[:danger] = "Please log in."
           redirect_to login_url
         end
@@ -53,6 +53,8 @@ class UsersController < ApplicationController
       # 正しいユーザーかどうか確認
       def correct_user
         @user = User.find(params[:id])
-        unless @user ==
+        unless current_user?(@user)
+          redirect_to root_url
+        end
       end
 end
